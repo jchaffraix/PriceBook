9d198582e5ee754385075b0c7cc32f69d86a2131
Julien Chaffraix <julien.chaffraix@gmail.com>
2020-05-02 18:23:46 -0700

    Minimal app engine app
    This adds the missing bits to deploy the app.
    Also adds the dependency on the datastore for later.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pricebook.appspot.com/m/v2
+
+go 1.14
+
+require cloud.google.com/go/datastore v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
+  "context"
   "fmt"
   "log"
   "net/http"
   "os"
+
+  "cloud.google.com/go/datastore"
 )
 
-func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-    log.Printf("Defaulting to port %s", port)
-  }
-
-  http.HandleFunc("/", defaultHandler)
-
-  if err := http.ListenAndServe(":"+port, nil); err != nil {
-    log.Fatal(err)
-  }
+type defaultHandler struct {
+  client *datastore.Client
 }
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func (defaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
     http.NotFound(w, r)
     return
@@ -29,3 +22,30 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
   fmt.Fprintf(w, "Hello World!")
 }
+
+func main() {
+  ctx := context.Background()
+
+  // Set your Google Cloud Platform project ID.
+  projectID := "pricebook"
+
+  // Creates a client.
+  client, err := datastore.NewClient(ctx, projectID)
+  if err != nil {
+    log.Fatalf("Failed to create client: %v", err)
+  }
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+    log.Printf("Defaulting to port %s", port)
+  }
+
+  http.Handle("/", defaultHandler{client})
+
+  if err := http.ListenAndServe(":"+port, nil); err != nil {
+    log.Fatal(err)
+  }
+}
+
+
